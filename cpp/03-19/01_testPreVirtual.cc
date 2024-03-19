3b6e96c12f20e80150b2478d95238482e8d11eab
lingang <qq1812159202@163.com>
2024-03-19 16:56:45 +0800

    测试纯虚函数

---
+++ cpp/03-19/01_testPreVirtual.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Base {
+public:
+    virtual void print1() {
+        cout << "Base -> print1()" << endl;
+    }
+    virtual void print2() = 0;
+private:
+
+
+};
+
+void Base::print2() {
+    cout << "Base -> print2()" << endl;
+}
+
+class Derive : public Base {
+public:
+    void print1() override {
+        cout << "Derive -> print1()" << endl;
+    }
+    void print2() override {
+        cout << "Derive -> print2()" << endl;
+    }
+private:
+
+};
+
+void test0() {
+    Derive d1;
+    d1.print1();
+    d1.print2();
+    d1.Base::print2();
+}
+
+int main(void) {
+    test0();
+    return 0;
+}
+
