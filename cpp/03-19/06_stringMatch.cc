b67a023b7565da597e0255b4b870e45b9c2e6b36
lingang <qq1812159202@163.com>
2024-03-19 23:09:40 +0800

    字符串匹配

---
+++ cpp/03-19/06_stringMatch.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+
+bool stringMatch(double k, const string &a, const string &b) {
+    if (a.length() == 0 || b.length() == 0) {
+        return false;
+    }
+    double count = 0; 
+    for (int i = 0; i < a.length(); i++) { // a和b长度相等，所以只判断a
+        if (a[i] == b[i]) {
+            ++count;
+        }
+    }
+    cout << (count / a.length()) << endl;
+    return k <= (count / a.length());
+}
+
+void test0() {
+    double k;
+    string a, b;
+    cin >> k;
+    cin >> a;
+    cin >> b;
+    cout << (stringMatch(k, a, b) ? "yes" : "no") << endl;
+}
+
+int main(void) {
+    auto begin = chrono::high_resolution_clock::now();
+    test0();
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end -begin);
+    cout << "用时: " << duration.count() << "毫秒" << endl;
+    return 0;
+}
+
