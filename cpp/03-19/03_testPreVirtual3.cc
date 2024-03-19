81e006deeab5401dc78a1d140e24f54e1507b53e
lingang <qq1812159202@163.com>
2024-03-19 17:41:31 +0800

    虚析构函数

---
+++ cpp/03-19/03_testPreVirtual3.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Base {
+public:
+    Base(const char *name) : _name(new char[strlen(name) + 1]()) {
+        cout << "Base(const char *name)" << endl;
+        strcpy(_name, name);
+    }
+    virtual void print1() {
+        cout << "Base -> print1()" << endl;
+    }
+    virtual void print2() = 0;
+    ~Base() {
+        delete [] _name;
+        cout << "~Base()" << endl;
+    }
+private:
+    char *_name;
+};
+
+void Base::print2() {
+    cout << "Base -> print2()" << endl;
+}
+
+class Derive : public Base {
+public:
+    Derive(const char *name, const char *sex) 
+    : Base(name) 
+    , _sex(new char[strlen(sex) + 1])
+    {
+        cout << "Derive(const char *name)" << endl;
+        strcpy(_sex, sex);
+    }
+    void print1() override {
+        cout << "Derive -> print1()" << endl;
+    }
+    void print2() override {
+        cout << "Derive -> print2()" << endl;
+    }
+    ~Derive() {
+        delete [] _sex;
+        cout << "~Derive()" << endl; 
+    }
+private:
+    char *_sex;
+};
+
+void test0() {
+    Derive d1("tom", "man");
+    d1.print1();
+    d1.print2();
+    d1.Base::print2();
+}
+
+int main(void) {
+    test0();
+    return 0;
+}
+
