653f53b4f630060b13ff9c06bcd6f7a5d286b1c3
lingang <qq1812159202@163.com>
2024-03-21 10:25:09 +0800

    update

--- cpp/03-21/01_templateFun.cc
+++ cpp/03-21/01_templateFun.cc
@@ -12,12 +12,29 @@ template <typename T>
 T add(T a, T b) {
     return a + b;
 }
+template <typename T1, typename T2>
+T1 add(T1 a, T2 b) {
+    cout << "template 1" << endl;
+    return a + b;
+}
+template <typename T1, typename T2>
+T1 add(T2 a, T1 b) {
+    cout << "template 2" << endl;
+    return a + b;
+}
 
 void test0() {
     int ia = 3, ib = 3;
     double da = 4.5, db = 7.9;
     cout << add(ia, ib) << endl;
     cout << add(da, db) << endl;
+    // cout << add<int>(ia, da) << endl;
+    // cout << add<int>(da, ia) << endl;
+    // cout << add<double, int>(da, ia) << endl;
+    // cout << add<double, int>(ia, da) << endl;
+    cout << add<double, int>(ia, da) << endl;
+    cout << add<int, double>(da, ia) << endl;
+
 }
 
 int main(void) {
