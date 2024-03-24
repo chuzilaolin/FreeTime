cc3520e9688e170c1c121067ce3533ad42d64a1e
lingang <qq1812159202@163.com>
2024-03-24 21:21:28 +0800

    praseXml

--- cpp/03-24/01_praseXml.cc
+++ cpp/03-24/01_praseXml.cc
@@ -5,6 +5,8 @@
 #include <string>
 #include <algorithm>
 #include <chrono>
+#include <regex>
+#include <fstream>
 #include "tinyxml2.h"
 
 using namespace std;
@@ -16,7 +18,16 @@ struct RssItem
     string link;
     string description;
     string content;
+    RssItem();
+    RssItem(string title, string link, string description, string content);
 };
+RssItem::RssItem() {}
+RssItem::RssItem(string title, string link, string description, string content) 
+: title(title)
+, link(link)
+, description(description)
+, content(content)
+{}
 
 class RssReader {
 public:
@@ -32,16 +43,56 @@ RssReader::RssReader(size_t size) {
 }
 
 void RssReader::parseRss(const string &filename) {
-    
+    // 打开xml
+    XMLDocument doc;
+    doc.LoadFile(filename.c_str());
+    if (doc.ErrorID()) {
+        cerr << "load file fail" << endl;
+        return;
+    }
+    // 开始读取文件
+    XMLElement *itemNode = doc.FirstChildElement("rss")->FirstChildElement("channel")->FirstChildElement("item");
+    while (itemNode) {
+        // 获取数据
+        string title = itemNode->FirstChildElement("title")->GetText();
+        string link = itemNode->FirstChildElement("link")->GetText();
+        string description = itemNode->FirstChildElement("description")->GetText();
+        string content = itemNode->FirstChildElement("content:encoded")->GetText();
+        // 处理html标签
+        regex reg("<[^>]*>");
+        description = regex_replace(description, reg, "");
+        content = regex_replace(content, reg, "");
+        // 存入vector
+        RssItem rssItem(title, link, description, content);
+        _rss.push_back(rssItem);
+        // 下一个结点
+        itemNode = itemNode->NextSiblingElement("item");
+    }
 }
 
 void RssReader::dump(const string &filename) {
-
+    ofstream ofs(filename);
+    if (!ofs) {
+        cerr << "open file is error" << endl;
+        return; 
+    }
+    int idx = 0;
+    for (const RssItem &item : _rss) {
+        ofs << "<doc><docid>" << ++idx
+            << "</docid><title>" << item.title
+            << "</title><link>" << item.link
+            << "</link><description>" << item.description
+            << "</description><content>" << item.content
+            << "</content></doc>" << endl;
+    }
+    ofs.close();
 }
 
 
 void test0() {
-
+    RssReader rr(4000);
+    rr.parseRss("./coolshell.xml");
+    rr.dump("./pagelib.dat");
 }
 
 int main(void) {
