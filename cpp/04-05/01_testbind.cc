7134bb3a10a2a3814d223f941b6cf18e42cd8e95
lingang <qq1812159202@163.com>
2024-04-05 10:47:50 +0800

    test bind

--- cpp/04-05/01_testbind.cc
+++ cpp/04-05/01_testbind.cc
@@ -16,15 +16,37 @@ void func(int a, int b, int c, int d) {
         << endl;
 }
 
+int func1(int a, int b, int c, int d) {
+    cout << "a = " << a << ", b = " << b 
+        << ", c = " << c
+        << ", d = " << d << endl;
+    return 0;
+}
+
 void test0() {
     using namespace std::placeholders;
     auto f = bind(func, 2, _3, _1, _2);
     f(5, 9, 11);
 }
 
+void test1() {
+    using namespace std::placeholders;
+    function<void(int)> f = bind(func, 2, _1, _1, _1);
+    f(5);
+}
+
+void test2() {
+    using namespace std::placeholders;
+    function<int(int)> f = bind(func1, 7, _1, _1, _1);
+    f(3);
+}
+
 int main(void) {
     auto begin = chrono::high_resolution_clock::now();
+#if 0
     test0();
+#endif
+    test2();
     auto end = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(end -begin);
     cout << "用时: " << duration.count() << "毫秒" << endl;
