00de8c24ce50170937e9fa661fe1d2c3ce9ebe95
lingang <qq1812159202@163.com>
2024-04-16 22:38:08 +0800

    workfow reids use

---
+++ cpp/04-16/02_workflowRedis.cc
@@ -0,0 +1,53 @@
+#include <func.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <chrono>
+#include <workflow/WFFacilities.h>
+
+
+using std::cout;
+using std::endl;
+
+static WFFacilities::WaitGroup gWaitGroup(1);
+void handler(int signum) {
+    gWaitGroup.done();
+}
+void test0() {
+    signal(SIGINT, handler);
+    WFRedisTask *redisTask = WFTaskFactory::create_redis_task("redis://8.137.12.216:6379", 0, [](WFRedisTask *redisTask){
+        protocol::RedisRequest *req = redisTask->get_req();
+        protocol::RedisResponse *resp = redisTask->get_resp();
+        int state = redisTask->get_state();
+        int error = redisTask->get_error();
+        switch(state) {
+            case WFT_STATE_SYS_ERROR:
+                cout << "sys error" << strerror(error) << endl;
+                break;
+            case WFT_STATE_DNS_ERROR:
+                cout << "dns error" << gai_strerror(error) << endl;
+                break;
+            case WFT_STATE_SUCCESS:
+                cout << "success" << endl;
+                break;
+        }
+    });
+    protocol::RedisRequest *req = redisTask->get_req();
+    req->set_request("set", {"name", "tom"});
+    redisTask->start();
+    cout << "main......\n";
+    gWaitGroup.wait(); 
+}
+
+int main(void) {
+    auto begin = std::chrono::high_resolution_clock::now();
+    test0();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end -begin);
+    cout << "用时: " << duration.count() << "毫秒" << endl;
+    return 0;
+}
+
