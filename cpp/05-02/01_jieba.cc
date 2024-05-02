86d49b0bd37b256556f31d427c0f15e79207f3ea
lingang <qq1812159202@163.com>
2024-05-02 14:32:18 +0800

    jieba分词

---
+++ cpp/05-02/01_jieba.cc
@@ -0,0 +1,43 @@
+#include <func.h>
+#include "include/Jieba.hpp"
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <chrono>
+#include <workflow/WFFacilities.h>
+
+using namespace std;
+
+const char* const DICT_PATH = "./dict/jieba.dict.utf8";
+const char* const HMM_PATH = "./dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "./dict/user.dict.utf8";
+const char* const IDF_PATH = "./dict/idf.utf8";
+const char* const STOP_WORD_PATH = "./dict/stop_words.utf8";
+
+void test0() {
+    string str = "我来到北京清华大学";
+    cppjieba::Jieba jieba(DICT_PATH,
+                          HMM_PATH,
+                          USER_DICT_PATH,
+                          IDF_PATH,
+                          STOP_WORD_PATH);
+    vector<string> words;
+    jieba.Cut(str, words, true);
+
+    for (const string & word : words) {
+        cout << word << "\n";
+    }
+}
+
+int main(void) {
+    auto begin = std::chrono::high_resolution_clock::now();
+    test0();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end -begin);
+    cout << "用时: " << duration.count() << "毫秒" << endl;
+    return 0;
+}
+
