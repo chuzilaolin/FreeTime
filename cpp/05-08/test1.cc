69b92873f783dee8c59b13f640c3fe5b3373659c
lingang <qq1812159202@163.com>
2024-05-08 08:22:34 +0800

    share_ptr move

---
+++ cpp/05-08/test1.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <memory>
+
+int main() {
+    std::shared_ptr<int> p1(new int(10));
+    std::shared_ptr<int> p2 = std::move(p1);
+    std::cout << "p2: " << *p2 << std::endl;
+    std::cout << "p1: " << *p1 << std::endl; // error
+    return 0;
+}
