02fd708c91eeb8f9b8a3bb725b75d212808d372c
lingang <qq1812159202@163.com>
2024-05-21 18:11:25 +0800

    05-21

---
+++ cpp/05-21/01test.cc
@@ -0,0 +1,25 @@
+#include <func.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <chrono>
+#include <workflow/WFFacilities.h>
+
+using namespace std;
+
+void test0() {
+    cout << "05-21" << endl;
+}
+
+int main(void) {
+    auto begin = std::chrono::high_resolution_clock::now();
+    test0();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end -begin);
+    cout << "用时: " << duration.count() << "毫秒" << endl;
+    return 0;
+}
+
